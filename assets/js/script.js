5c43fdbe808c2b01ee6ed87f98cc200a27e46a87
Mateus <mateusvaldejao@Mateuss-MacBook-Pro.local>
2022-06-29 19:49:01 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -149,16 +149,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function (event) {
     var tempArr = [];
@@ -187,15 +189,22 @@ $("#trash").droppable({
   drop: function (event, ui) {
     console.log("drop");
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-class-active")
   },
   over: function (event, ui) {
-    console.log("over")
+    $(".bottom-trash").addClass("bottom-trash-active")
   },
   out: function (event, ui) {
-    console.log("out")
+    $(".bottom-trash").removeClass("bottom-trash-active")
   }
 
 })
+
+setInterval(function () {
+  $(".card .list-group-item").each(function (index, el) {
+    auditTask(el);
+  });
+}, 1800000)
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -214,7 +223,7 @@ $("#modalDueDate").datepicker({
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
